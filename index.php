254c284c38b61c54275002454c2c71c321e7754a
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-05-23 20:22:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -14,22 +14,74 @@ use App\Controller\InputController;
 define('BASE_PATH', __DIR__);
 define('BASE_URL', baseUrl());
 
+$products = [
+    [
+        'name'  => 'Minimalist Leather Backpack',
+        'qty'   => 1,
+        'price' => 120,
+    ],
+    [
+        'name'  => 'Wireless Noise-Canceling Headphones',
+        'qty'   => 1,
+        'price' => 250,
+    ],
+    [
+        'name'  => 'Smart Fitness Watch',
+        'qty'   => 1,
+        'price' => 199,
+    ],
+    [
+        'name'  => 'Portable Bluetooth Speaker',
+        'qty'   => 1,
+        'price' => 89,
+    ],
+];
+$shippingCost = 10;
+
+$data = [
+    'products' => $products,
+    'shipping_cost' => $shippingCost,
+    'address' => [
+        'name' => 'Sherlock Holmes',
+        'email' => 'sherlock@example.com',
+        'address' => '221B Baker Street, London, England',
+        'city' => 'London',
+        'post_code' => 'NW16XE',
+    ]
+];
+
 
 Routes::get('/', function () {
     return view('app', []);
 });
 
-Routes::get('/checkout', function () {
-    return view('checkout', []);
-});
 
+Routes::get('/checkout', function () use($data) {
+    return view('checkout', $data);
+});
 
 Routes::post('/checkout', function () {
-
-    $validInput = InputController::inputValidate($_POST);
-
-    PaymentController::createSession();
+    $data       = json_decode(file_get_contents("php://input"), true);
+    $validInput = InputController::inputValidate($data);
+    PaymentController::createSession($validInput);
 });
+
+Routes::get('/success', function () {
+    return PaymentController::success();
+});
+
+Routes::get('/failure', function () {
+    return PaymentController::failure();
+});
+
+Routes::get('/verify-payment', function () {
+     PaymentController::verifyTransaction($_GET['session_id'] ?? null);
+});
+
+Routes::get('/order-confirmation', fn() =>
+    PaymentController::orderConfirmation($_GET['session_id'] ?? null)
+);
+
 
 $routes = Routes::getInstance();
 $routes->dispatch();
